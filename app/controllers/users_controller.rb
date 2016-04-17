2b1cefee227487fb2dcec3c4699740e2e81155ab
yuji y united <yuji-yamagata@united.jp>
2016-04-17 08:36:13 +0000

    first commit for user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  
   def new
     @user = User.new
   end
   
   def show
-    @user = User.find(params[:id])
   end
+  
+  def edit
+  end
+  
   
   def create
     @user = User.new(user_params)
@@ -21,6 +26,10 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
 end
