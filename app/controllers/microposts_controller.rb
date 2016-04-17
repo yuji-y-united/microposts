39bed61bbea15c73b1b5104f320a761808436ea9
yuji y united <yuji-yamagata@united.jp>
2016-04-17 11:39:35 +0000

    add posing microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-    before_action :logged_in_user, only: [:create]
+    before_action :logged_in_user, only: [:create, :destroy]
     
     def create
         @micropost = current_user.microposts.build(micropost_params)
@@ -10,6 +10,15 @@ class MicropostsController < ApplicationController
             render 'static_pages/home'
         end
     end
+    
+    def destroy
+        @micropost = current_user.microposts.find_by(id: params[:id])
+        return redirect_to root_url if @micropost.nil?
+        @micropost.destroy
+        flash[:success] = "Micropost deleted"
+        redirect_to request.referrer || root_url
+    end
+        
     
     private
     def micropost_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def edit
