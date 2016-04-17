fbf4929775055e10e7a1022bd31972cafe6972ea
yuji y united <yuji-yamagata@united.jp>
2016-04-17 10:29:18 +0000

    drop table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415063824) do
+ActiveRecord::Schema.define(version: 20160417100945) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
