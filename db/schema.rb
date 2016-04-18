442ae98dc07b3c1f7ea0b073b6395ca6dce009b5
yuji y united <yuji-yamagata@united.jp>
2016-04-18 02:20:54 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417121220) do
+ActiveRecord::Schema.define(version: 20160418013926) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20160417121220) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.text     "profmessage"
+    t.text     "area"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
